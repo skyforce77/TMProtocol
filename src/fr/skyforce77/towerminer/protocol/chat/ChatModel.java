7fadd70953b786c5b8c69b7c7326700c69d6c501
skyforce77 <arnaud.bruyer@gmail.com>
2014-09-05 18:43:45 +0200

    added new chat formats

--- src/fr/skyforce77/towerminer/protocol/chat/ChatModel.java
+++ src/fr/skyforce77/towerminer/protocol/chat/ChatModel.java
@@ -6,6 +6,7 @@ public class ChatModel extends MessageModel{
 	private static final long serialVersionUID = 4659887846514541L;
 
 	private MessageModel mousemodel;
+	private boolean islink;
 	
 	public ChatModel(String text) {
 		super(text);
@@ -22,6 +23,14 @@ public class ChatModel extends MessageModel{
 	
 	public MessageModel getMouseModel() {
 		return mousemodel;
+	}
+	
+	public boolean isLink() {
+		return islink;
+	}
+	
+	public void setLink(boolean islink) {
+		this.islink = islink;
 	}
 
 }
--- src/fr/skyforce77/towerminer/protocol/chat/MessageModel.java
+++ src/fr/skyforce77/towerminer/protocol/chat/MessageModel.java
@@ -12,6 +12,10 @@ public class MessageModel implements Serializable{
 	private Color highlight = null;
 	private String text = "";
 	private String option = null;
+	private boolean striked = false;
+	private boolean underlined = false;
+	private boolean italic = false;
+	private boolean bold = false;
 	
 	public MessageModel() {}
 	
@@ -67,6 +71,38 @@ public class MessageModel implements Serializable{
 	
 	public String getOption() {
 		return option;
+	}
+	
+	public boolean isStriked() {
+		return striked;
+	}
+	
+	public boolean isUnderlined() {
+		return underlined;
+	}
+	
+	public boolean isItalic() {
+		return italic;
+	}
+	
+	public boolean isBold() {
+		return bold;
+	}
+	
+	public void setStriked(boolean striked) {
+		this.striked = striked;
+	}
+	
+	public void setUnderlined(boolean underlined) {
+		this.underlined = underlined;
+	}
+	
+	public void setItalic(boolean italic) {
+		this.italic = italic;
+	}
+	
+	public void setBold(boolean bold) {
+		this.bold = bold;
 	}
 
 }
